efa1a58df3f912d1a571aaaf628c0e68a8983a4a
Alfredo.Vidinhas <alfredo.vidinhas@tsco.co.ao>
2023-08-22 15:49:57 +0100

    Configuracao de arquivos

--- src/util.ts
+++ src/util.ts
@@ -18,3 +18,26 @@ const removeAngularRoot = (url: string) => {
     const regex = new RegExp('^.+?src' + sep + sep + 'app' + sep + sep,"g");
     return url.replace(regex, '');
 };
+
+const configFiles = (url: string) => {
+    const name = path.dirname(url);
+    const facadeDestinationUrl = url + path.sep + name + '.facade.ts';
+    const stateDestinationUrl = url + path.sep + 'state' + path.sep + name + '.facade.ts';
+    //fs.copyFileSync('examples' + path.sep + 'state.ts', );
+
+    
+    fs.readFile('examples' + path.sep + 'facade.ts', { encoding: 'utf8' }, (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        data = data.replace(/todo/gi, name);
+
+        fs.writeFile(facadeDestinationUrl, data, (err) => {
+            if (err) {
+                console.error(err);
+            }
+        });
+    });
+};
